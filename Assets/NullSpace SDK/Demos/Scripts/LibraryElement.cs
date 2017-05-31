9bcd503f0b91e6e603f1aec22e361973b2127178
Casey Waldren <casey@nullspacevr.com>
2017-05-31 13:41:47 -0700

    Add release dlls for next release

--- Assets/NullSpace SDK/Demos/Scripts/LibraryElement.cs
+++ Assets/NullSpace SDK/Demos/Scripts/LibraryElement.cs
@@ -190,6 +190,14 @@ namespace NullSpace.SDK.Demos
 					{
 						MarkElementBroken();
 					}
+
+					//string targetDirectory = myPackage.path + " - Converted";
+
+					//playResult = OpenFile(targetDirectory);
+					//if (!playResult)
+					//{
+					//	MarkElementBroken();
+					//}
 				});
 			}
 		}

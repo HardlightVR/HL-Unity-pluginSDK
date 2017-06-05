b6aa5c796c399976749ccdb1f2e4606a62dbd677
Jonathan Kirk Palmer <vantharion@gmail.com>
2017-06-05 15:14:52 -0700

    Merge of the Volume Control options.

--- Assets/NullSpace SDK/Scripts/NSManager.cs
+++ Assets/NullSpace SDK/Scripts/NSManager.cs
@@ -162,7 +162,27 @@ namespace NullSpace.SDK
 			return _plugin.SampleCurrentlyPlayingEffects();
 			//return _plugin.SampleStrengths();
 		}
+		/// <summary>
+		/// Control the volume of an area directly. 
+		/// </summary>
+		/// <param name="singleArea">An AreaFlag representing a single area</param>
+		/// <param name="strength">Strength to play, from 0-255</param>
+		public void ControlDirectly(AreaFlag singleArea, double strength)
+		{
+			_plugin.ControlDirectly(singleArea, strength);
+		}
 
+		/// <summary>
+		/// Control the volume of multiple areas directly. 
+		/// </summary>
+		/// <param name="singleAreas">List of AreaFlags, each representing a single area</param>
+		/// <param name="strengths">Strength to play, from 0-255</param>
+		public void ControlDirectly(AreaFlag[] singleAreas, ushort[] strengths)
+		{
+			_plugin.ControlDirectly(singleAreas, strengths);
+
+		}
+	
 		/// <summary>
 		/// Tell the manager to use a different IMU calibrator
 		/// </summary>
@@ -311,6 +331,7 @@ namespace NullSpace.SDK
 		{
 			this.EnableTracking();
 		}
+
 
 
 		private IEnumerator UpdateTracking()
